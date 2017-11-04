f0c553d47c6513e3c2ac3568b1c9170ae9aa94fb
marcin-szczepanski <marcin.szczepanski1995@gmail.com>
2017-11-04 20:35:09 +0100

    Modify components

--- Ksiegarnia/src/app/app.component.ts
+++ Ksiegarnia/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  mode = null;
+  
+  books = null;
+
+  mode = 0;
+
+  constructor(private http: Http) {
+
+  }
+
+
+search(query, callback) {
+  let url = "http://localhost:8080/author";
+  this.http.get(url).subscribe((response: Response)=>{
+    let data = response.json();
+    let books = data;
+    this.books = data;
+    callback(books);
+  })
+}
+
+getBooks(callback) {
+  let query = "Query...";
+  this.search(query, callback);
+  console.log(this.books);  
+}
+
 }
