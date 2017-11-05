f7846c26ed89406f6ab35b2ee4ff06d0cb47efb7
Drizzet <dawid.bogacki@gmail.com>
2017-11-05 19:26:47 +0100

    adding Cross

--- hello/src/main/java/hello/AuthorController.java
+++ hello/src/main/java/hello/AuthorController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthorController {
 	
+	@CrossOrigin
 	@RequestMapping("/author")
 	public ArrayList<Author> author() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
