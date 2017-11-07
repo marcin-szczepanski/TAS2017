bfd20dad918d5b284facafd9fcef9e65273ab5ed
Drizzet <dawid.bogacki@gmail.com>
2017-11-07 14:14:17 +0100

    TOP6 poprawka2

--- hello/src/main/java/hello/TOP6DAO.java
+++ hello/src/main/java/hello/TOP6DAO.java
@@ -16,15 +16,7 @@ public class TOP6DAO {
    }
 
    public ArrayList<TOP6> listTOP6() {
-      String SQL = "CREATE OR ALTER VIEW TOP6(Nazwa,Imie_autora,Nazwisko_autora,Ocena,Kategoria,Cena)\r\n" + 
-      		"as\r\n" + 
-      		"SELECT TOP 6 k.Nazwa,a.Imie,a.Nazwisko,k.Ocena,t.Kat,k.Cena\r\n" + 
-      		"FROM Ksiazka k\r\n" + 
-      		"	JOIN Kategoria t ON k.Kategoria = t.id\r\n" + 
-      		"	JOIN Autor a ON k.Autor = a.id\r\n" + 
-      		"ORDER BY k.ocena DESC\r\n" + 
-      		"\r\n" + 
-      		"Select * from TOP6";
+      String SQL = "Select * from TOP6";
       ArrayList <TOP6> top6 = (ArrayList<TOP6>) jdbcTemplateObject.query(SQL, new TOP6Mapper());
       return top6;
    }
