a65c97e71bc320ef56e08e2fa2746761c983b87a
marcin-szczepanski <marcin.szczepanski1995@gmail.com>
2017-11-12 16:21:13 +0100

    Add TOP6

--- Ksiegarnia/src/app/newest/newest.component.ts
+++ Ksiegarnia/src/app/newest/newest.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-newest',
@@ -7,42 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewestComponent implements OnInit {
 
-  constructor() { }
+  books = [];
+  answer = {};
+
+  constructor(private http: Http) { 
+    this.getBooks();
+  }
 
   ngOnInit() {
   }
 
-  books = [
-    {
-      title: "title",
-      author: "author",
-      price: "24.99 zł"
-    },
-    {
-      title: "title",
-      author: "author",
-      price: "24.99 zł"
-    },
-    {
-      title: "title",
-      author: "author",
-      price: "24.99 zł"
-    },
-    {
-      title: "title",
-      author: "author",
-      price: "24.99 zł"
-    },
-    {
-      title: "title",
-      author: "author",
-      price: "24.99 zł"
-    },
-    {
-      title: "title",
-      author: "author",
-      price: "24.99 zł"
-    }
-  ];
+  getBooks() {
+    const url = "http://localhost:8080/top6";
+    const data = this.getService(url)
+    .then(answer => this.addBooks(answer));
+  }
 
+  getService(url: string): Promise<any> {
+    return this.http
+        .get(url)
+        .toPromise()
+        .then(res => this.extractData(res));
+  }
+
+  private extractData(res: Response) {
+    this.answer = res.json();;
+    return res.json() || {};
+  }
+
+  addBooks(answer) {
+    for (let i = 0; i < answer.length; i++) {
+      const book = {};
+      // book['id'] = ???;
+      book['title'] = answer[i].nazwa;
+      book['author'] = answer[i].imie + ' ' + answer[i].nazwisko;
+      book['price'] = answer[i].cena;
+      this.books.push(book);
+    }
+  }
 }
