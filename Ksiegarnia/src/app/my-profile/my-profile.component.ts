66c34927025061ac51e1730e457c9c058da22473
LukaszLejman <LukaszLejman96@wp.pl>
2017-11-13 23:02:08 +0100

    szkielet

--- Ksiegarnia/src/app/my-profile/my-profile.component.ts
+++ Ksiegarnia/src/app/my-profile/my-profile.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent implements OnInit {
-
+  login = 'jakislogin123';
+  email = 'jakisemail123@costam.pl';
+  imie = 'jakiesimie';
+  nazwisko = 'jakiesnazwisko';
+  telefon = '997';
+  ulica = 'jakasulica';
+  miasto = 'jakiesmiesto';
+  kodPocztowy = '12-123';
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
