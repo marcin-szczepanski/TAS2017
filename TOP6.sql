41645c50d9bc7a92674b58bc604f85a2179d584f
Kublen <jakub.lenartowicz@gmail.com>
2017-11-15 15:57:40 +0100

    Delete TOP6.sql

--- TOP6.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR ALTER VIEW TOP6(Id,Nazwa,Imie_autora,Nazwisko_autora,Ocena,Kategoria,Cena)
-as
-SELECT TOP 6 k.id,k.Nazwa,a.Imie,a.Nazwisko,k.Ocena,t.Kat,k.Cena
-FROM Ksiazka k
-	JOIN Kategoria t ON k.Kategoria = t.id
-	JOIN Autor a ON k.Autor = a.id
-ORDER BY k.ocena DESC
-
-Select * from TOP6
