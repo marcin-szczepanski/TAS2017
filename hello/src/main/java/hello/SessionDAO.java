f1fb3d24192dc0aac198c55f35195cf953999b6d
LukaszLejman <LukaszLejman96@wp.pl>
2017-11-20 16:42:33 +0100

    Session fix

--- hello/src/main/java/hello/SessionDAO.java
+++ hello/src/main/java/hello/SessionDAO.java
@@ -17,7 +17,7 @@ public void setDataSource(DataSource dataSource) {
 public String createSession(String login, String password) {
     String SQL = "select login,haslo,imie,status from Uzytkownik where login='"+login+"'";
     User user = ((ArrayList<User>) jdbcTemplateObject.query(SQL, new UserMapper())).get(0);
-    if(user.getPassword()==password) {
+    if(user.getPassword().equals(password)) {
     	String SQL2="UPDATE Uzytkownik SET status='1' WHERE login='"+login+"'";
     	jdbcTemplateObject.update(SQL2);
     	return "Witaj, "+user.getName();
