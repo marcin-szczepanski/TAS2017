a20bc33480232fc7022499d432be93c8a00de8f4
Drizzet <dawid.bogacki@gmail.com>
2017-11-26 00:02:57 +0100

    Update - fix
    jdbcTemplateObject.update(SQL);

--- hello/src/main/java/update/UpdateDAO.java
+++ hello/src/main/java/update/UpdateDAO.java
@@ -16,7 +16,7 @@ public class UpdateDAO {
 	
 	public String updateDatabase(String what, String how, String who) {
 		String SQL = "UPDATE Uzytkownik SET "+what+"='"+how+"' WHERE id="+who+"";
-		Update user = ((ArrayList<Update>) jdbcTemplateObject.query(SQL, new UpdateMapper())).get(0);
+		jdbcTemplateObject.update(SQL);
 		return "Zaktualizowano";
 	}
 }
