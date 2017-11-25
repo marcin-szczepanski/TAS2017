6bdf1f20edda430537d05d3a8cb9dd4e2d47c7ee
LukaszLejman <LukaszLejman96@wp.pl>
2017-11-25 22:43:55 +0100

    profil

--- Ksiegarnia/src/app/my-profile/my-profile.component.ts
+++ Ksiegarnia/src/app/my-profile/my-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MyProfileServiceService } from './my-profile-service.service';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-my-profile',
@@ -9,41 +10,52 @@ import { Subject } from 'rxjs/Subject';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent implements OnInit {
-  login = 'jakislogin123';
-  email = 'jakisemail123@costam.pl';
-  imie = 'jakiesimie';
-  nazwisko = 'jakiesnazwisko';
-  telefon = '997';
-  ulica = 'jakasulica';
-  miasto = 'jakiesmiesto';
-  kodPocztowy = '12-123';
+  login = '';
+  email = '';
+  imie = '';
+  nazwisko = '';
+  telefon = '';
+  ulica = '';
+  miasto = '';
+  kodPocztowy = '';
   results;
-  loadUserInfo() {
-    const subscription = this.MyProfileServiceService.showMyProfile().subscribe(
-      function (response) {
-        this.results = response.json();
-        load(this.results);
-      },
-      function (error) { },
-      function () {
-        this.login = this.results[0].id;
-        console.log(this);
-      }
+  // loadUserInfo() {
+  //   const subscription = this.MyProfileServiceService.showMyProfile().subscribe(
+  //     function (response) {
+  //       this.results = response.json();
+  //       load(this.results);
+  //     },
+  //     function (error) { },
+  //     function () {
+  //       this.login = this.results[0].id;
+  //       console.log(this);
+  //     }
 
-    );
-    function load(results) {
-      console.log(results[0].id);
+  //   );
+  //   function load(results) {
+  //     console.log(results[0].id);
 
-    }
-  }
-  constructor(private MyProfileServiceService: MyProfileServiceService) {
-    this.loadUserInfo();
-  }
+  //   }
+  // }
+  constructor(private http: Http) { }
 
 
 
   ngOnInit() {
-
+    const url = 'http://localhost:8080/profile?id=' + sessionStorage.getItem('id');
+    console.log(url);
+    this.http.get(url).subscribe(data => {
+      this.results = data.json();
+      this.login = this.results[0].login;
+      this.email = this.results[0].email;
+      this.imie = this.results[0].imie;
+      this.nazwisko = this.results[0].nazwisko;
+      this.telefon = this.results[0].telefon;
+      this.ulica = this.results[0].adres;
+      this.miasto = this.results[0].miasto;
+      this.kodPocztowy = this.results[0].kod;
+    });
+    ;
   }
 
 
