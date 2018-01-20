33710af514a4f69bb74e5748d9e4d9a3f6c8b880
Kublen <jakub.lenartowicz@gmail.com>
2018-01-20 13:14:31 +0100

    Update TOP6.sql

--- TOP6.sql
+++ TOP6.sql
@@ -2,5 +2,5 @@
 
 SELECT DISTINCT TOP 6  b.*,AVG(o.ocena)
 FROM ALLBOOKS b JOIN Oceny o ON b.id = o.id_ks
-group by b.id,b.Imie_autora,b.Nazwisko_autora,b.Kategoria,b.Nazwa,b.Cena
+group by b.id,b.Imie_autora,b.Nazwisko_autora,b.Kategoria,b.Nazwa,b.Cena,b.Okladka
 ORDER BY AVG(o.ocena) DESC
