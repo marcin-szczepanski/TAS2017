099ce4fc4475d33f8e024dfe85ab617cf41b4197
Kublen <jakub.lenartowicz@gmail.com>
2018-01-20 12:54:05 +0100

    Update Okladki.sql

--- Okladki.sql
+++ Okladki.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Okladki(
 id int not null primary key,
 id_ks int references Ksiazka(id),
-odkladka text);
+okladka text);
 
 
 insert into Okladki values(1,5,'http://ecsmedia.pl/c/smakoterapia-w-iext50788129.jpg')
