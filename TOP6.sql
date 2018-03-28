68c32f6fedcf459cc0b2e9c24ff4144129d4782d
LukaszLejman <LukaszLejman96@wp.pl>
2018-03-28 15:11:58 +0200

    database

--- TOP6.sql
+++
@@ -1,4 +0,0 @@
-SELECT DISTINCT TOP 6  b.*,AVG(o.ocena) as Ocena
-FROM ALLBOOKS b JOIN Oceny o ON b.id = o.id_ks
-group by b.id,b.Imie_autora,b.Nazwisko_autora,b.Kategoria,b.Nazwa,b.Cena,b.Okladka
-ORDER BY AVG(o.ocena) DESC
